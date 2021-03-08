ffcf6876b994a20e83f4d405ed425dc65c8df4ee
RvDmitry <rvdmitry@gmail.com>
2021-03-08 22:47:44 +0600

    0. ToOne [#407075]

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,14 @@ create table cars(
     model varchar(255),
     created timestamp
 );
+
+create table j_role (
+                        id serial primary key,
+                        name varchar(2000)
+);
+
+create table j_user (
+                        id serial primary key,
+                        name varchar(2000),
+                        role_id int not null references j_role(id)
+);
